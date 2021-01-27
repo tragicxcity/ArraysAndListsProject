aa94d6aa68a87b0ce69d05045b1f6e45ac812ba1
Blake.Foster <camblakefost35007@gmail.com>
2021-01-27 13:20:57 -0600

    project

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ArraysAndLists
 {
@@ -9,7 +10,49 @@ namespace ArraysAndLists
             //TODO:
 
             // Create an int Array and populate numbers 1-10
+            int[] myArray01 = new int[10];
 
+            myArray01[0] = 1;
+            myArray01[1] = 2;
+            myArray01[2] = 3;
+            myArray01[3] = 4;
+            myArray01[4] = 5;
+            myArray01[5] = 6;
+            myArray01[6] = 7;
+            myArray01[7] = 8;
+            myArray01[8] = 9;
+            myArray01[9] = 10;
+
+            Console.WriteLine("evens");
+
+            {
+                var evens = new List<int>();
+
+                for (int a = 2; a < 12; a+=2)
+                {
+                    evens.Add(a);
+                }
+                foreach (var num in evens)
+                {
+                    Console.WriteLine(num);
+                }
+
+                Console.WriteLine();
+
+                Console.WriteLine("odds");
+
+                var odds = new List<int>();
+
+                for (int b = 1; b < 11; b+=1)
+                {
+                    odds.Add(b);
+                }
+                foreach (var num in odds)
+                {
+                    Console.WriteLine(num);
+                }
+
+            }
             /* Create two Lists of type int.
              * Name one List "evens"
              * Name the other List "odds"
